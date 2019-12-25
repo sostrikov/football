304d5be80ec5f04cf6e25b1d03a69380419e154f
ostrikov <ostrikov@bmstu.ru>
2019-12-25 21:47:08 +0300

    5.7

--- Football/Controllers/HomeController.cs
+++ Football/Controllers/HomeController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Football.Models;
+using System.Data.Entity;
 
 namespace Football.Controllers
 {
     public class HomeController : Controller
     {
+        FootballContext db = new FootballContext();
+        // Выводим всех футболистов
         public ActionResult Index()
         {
-            return View();
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+
+        public ActionResult TeamDetails(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Team team = db.Teams.Include(t => t.Players).FirstOrDefault(t => t.Id == id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
         }
 
         public ActionResult About()
